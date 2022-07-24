c69c69d6132ecedaf9b5670eaf4417b9c9b825f3
lihaosen <779427235@qq.com>
2022-07-24 20:33:25 +0800

    feat: 添加强缓存和协商缓存

--- my-node-server/src/index.js
+++ my-node-server/src/index.js
@@ -1,21 +1,35 @@
-require("./signIn");
-const path = require("path");
-const Koa = require("koa");
-const koaStatic = require("koa-static");
-const http = require("http");
-const socketInit = require("./socket");
-const routerInit = require("./router");
+require('./signIn')
+const path = require('path')
+const Koa = require('koa')
+const koaStatic = require('koa-static')
+const http = require('http')
+const socketInit = require('./socket')
+const routerInit = require('./router')
 
-const app = new Koa();
+const app = new Koa()
 
 // app.listen(...) creat 加 listen 的语法糖
-const server = http.createServer(app.callback());
+const server = http.createServer(app.callback())
 
-app.use(koaStatic(path.join(__dirname, "../public")));
+app.use(
+  koaStatic(path.join(__dirname, '../public'), {
+    setHeaders(res, path, stats) {
+      // 12小时的缓存
+      res.setHeader('cache-control', `max-age=${60 * 60 * 12}`)
+      const ifModifiedSince = res.req.headers['if-modified-since']
+      const lastModified = stats.mtime.toGMTString()
+      if (ifModifiedSince === lastModified) {
+        res.writeHead(304) // 虽然不建议但我还是用了
+      } else {
+        res.setHeader('Last-Modified', lastModified)
+      }
+    },
+  })
+)
 
-const io = socketInit(server);
-routerInit(app, io);
+const io = socketInit(server)
+routerInit(app, io)
 
 server.listen(8007, () => {
-  console.log("run at http://localhost:8007/");
-});
+  console.log('run at http://localhost:8007/')
+})
