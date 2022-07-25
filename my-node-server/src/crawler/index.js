b2928531879651e7a374af7055b05044923e13bd
lihaosen <779427235@qq.com>
2022-07-25 22:32:58 +0800

    feat: 爬虫爬取图片

--- my-node-server/src/crawler/index.js
+++ my-node-server/src/crawler/index.js
@@ -1,36 +1,90 @@
-const axios = require('./service')
+const axios = require('axios')
 const cheerio = require('cheerio')
+const sizeOf = require('image-size')
+const fs = require('fs')
+const path = require('path')
 
-function delay(time){
-  return new Promise((resolve, reject) =>{
-    setInterval(()=>{
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
       resolve()
     }, time)
   })
 }
 
-function getRanking() {
-  return axios({ url: '/ranking' })
+function getRankHTML() {
+  return axios({ url: 'https://www.vilipix.com/ranking' })
+}
+
+function getIllustHTML(url) {
+  return axios({ url: `https://www.vilipix.com${url}` })
+}
+
+function getResizedImg(url) {
+  return axios({
+    url,
+    params: { 'x-oss-process': `image/resize,w_200` },
+    responseType: 'arraybuffer',
+  })
 }
 
 async function ranking() {
-  const {data: html} = await getRanking()
+  // await delay(1000 * 60 * 3)
+  const { data: html } = await getRankHTML()
   const $ = cheerio.load(html)
-  const hrefs = $('.illust a').map(function(i, el){
+  const hrefs = $('.illust a').map(function () {
     return $(this).attr('href')
   })
 
-  hrefs.some((href)=>{
-    return await isWide(url)
-    
-  })
-  
-
+  for (let i = 0; i < hrefs.length; i++) {
+    console.log('i', i)
+    const url = await isWide(hrefs[i])
+    if (url) {
+      const convertedURL = convertImgURL(url)
+      try {
+        await downLoadPicture(convertedURL + '.png')
+      } catch {
+        await downLoadPicture(convertedURL + '.jpg')
+      }
+      return
+    }
+  }
 }
+
 // wide 返回链接，否则返回 false
-async function isWide(url){
-  await delay(500)
-  await axios({ url })
+async function isWide(url) {
+  // await delay(500)
+  const { data: html } = await getIllustHTML(url)
+  const $ = cheerio.load(html)
+  const imgDOMs = $('.illust-pages a img')
+  const imgDOM = imgDOMs.length > 1 ? imgDOMs[0] : imgDOMs
+  const src = $(imgDOM).attr('src')
+  const { data: img } = await getResizedImg(src)
+  const { height, width } = sizeOf(img)
+  if (width / height > 1.2) return src
+  return false
+}
+
+async function downLoadPicture(href) {
+  try {
+    const target_path = path.resolve(
+      __dirname,
+      `../../public/img/todayBackground.jpg`
+    )
+    const response = await axios.get(href, { responseType: 'stream' })
+    await response.data.pipe(fs.createWriteStream(target_path))
+    console.log('写入成功')
+    return Promise.resolve()
+  } catch (e) {
+    console.log('写入数据失败')
+    return Promise.reject(e)
+  }
+}
+
+function convertImgURL(imgURL) {
+  return imgURL
+    .replace(/\/regular\//, '/original/')
+    .replace(/_master1200.jpg\?x-oss-process=image\/resize,m_fill,w_1000/, '')
 }
 
 ranking()
--- my-node-server/src/crawler/service.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-const axiosInstance = axios.create({
-  baseURL: 'https://www.vilipix.com'
-});
-
-module.exports = axiosInstance
